ce49a9fee8c9b68959fb15024db952c9a75a94dc
Sam <sam-drew@outlook.com>
2017-06-13 11:20:18 +0100

    Method for stipping ids while stepping through videos now works, but it manual (user has to click the next button).

--- get_episodes.py
+++ get_episodes.py
@@ -1,26 +1,33 @@
 from selenium import webdriver
 import time
 
-def log_url(url):
+def log_id(ID):
     f = open("IDs.txt", "a")
-    f.write((str((url + "\n"))))
+    f.write((str((ID + "\n"))))
     f.close()
+    print("Logging ID: " + ID)
+
+def strip_id(url):
+    splitString = url.rsplit("?", 1)
+    splitString = splitString[0].rsplit("/")
+    ep_id = splitString[len(splitString) - 1]
+    return(ep_id)
 
 driver = webdriver.Chrome('/home/user/Downloads/chromedriver')
-driver.get("https://www.netflix.com/watch/80002472/")
+last_id = "80006978"
+driver.get("https://www.netflix.com/watch/80006978")
 
 # Really really hacky way of getting credentials set. User has 42 seconds to
 # log in and return to ep1 URL.
 time.sleep(40)
-print("I AM AWAKE. 2 seconds remaining.")
+print("I AM AWAKE.\n2 seconds remaining.\nMake sure you're on ep1 page.")
 time.sleep(2)
 
-# During this stage, the mouse must be kept active over the browser window.
-# Recommend having terminal on top and moving mouse between the 2 windows.
-for i in range(206):
-    time.sleep(3)
-    button = driver.find_element_by_xpath('//*[@id="netflix-player"]/div[4]/section[2]/div[5]/div[1]')
-    button.click()
-    log_url(driver.current_url)
+# User manualy clicks through the episodes until reaches S9E24. Logs as url
+# changes.
+while last_id != "80144357":
+    if strip_id(driver.current_url) != last_id:
+        log_id(strip_id(driver.current_url))
+        last_id = strip_id(driver.current_url)
 
 driver.quit()
